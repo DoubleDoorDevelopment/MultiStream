2cf789d03c2206f56b0d861dd8f8c3943dc7e061
Richard Campen <richard@campen.co>
2016-06-18 08:30:44 -0400

    Add filtering by game, update README accordingly

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MultiStream
 
-#### Version 1.0b3
+#### Version 1.0b4
 
 Flask app for fetching if selected Twitch streams are live, and if so redirecting to multistre.am for the live users.
 
@@ -15,7 +15,14 @@ To run this app locally for testing purposes use the `run.py` file which enables
 using [uWSGI](http://uwsgi-docs.readthedocs.io/en/latest/) and using it to run `app.py` directly.
 
 To use this app, go to the url that you host this app on and it should redirect you to a multistre.am multistream for the 
-streamers that you have specified in the config file. Optionally, streamers in the list can be filtered by the url e.g.:
-`example.website.com` will redirect to a multistre.am url for all streamers in the list that are live; 
-`example.website.com/d3` will redirect to a multistre.am url for all streamers in the list that are live that have the
-   string `d3` in their stream title.
+streamers that you have specified in the config file. Optionally, streamers in the list can be filtered (case insensitive):
+
+* `example.url/substring` will filter by streams containing the substring in their status. This is an alias for the
+advanced filtering option by status below.
+
+For advanced filtering:
+
+* `example.url/status/substring` will filter by streams containing the substring in their status
+* `example.url/game/string` will filter by streams whose game matches the string exactly
+
+Both these options can be aliased by their first letters so `example.url/s/substring` and `example.url/g/string`.
--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 
 """Flask app for fetching if selected Twitch streams are live, and redirecting to multistre.am for the live streams.
 
-MulstiStream version 1.0b3
+MultiStream version 1.0b4
 ==========================
 
 For complete documentation see README.md.
@@ -18,9 +18,8 @@ from flask import Flask, jsonify, redirect
 app = Flask(__name__)
 app.config.from_object('config')
 
-@app.route('/')
-@app.route('/<string:filter_>')
-def get_live(filter_=''):
+@app.route('/', methods=['GET'])
+def get_all_live():
 
     live_list = []
 
@@ -35,16 +34,90 @@ def get_live(filter_=''):
     for index, value in enumerate(response):
         data = value.json()
         if data['stream'] is not None:
-            if not filter_:
-                live_list.append(streamers[index])
-            elif filter_ in data['stream']['channel']['status']:
+            live_list.append(streamers[index])
+
+    new_url = 'http://multistre.am/%s' % ('/'.join(user for user in live_list))
+
+    return redirect(new_url, code=303)
+
+
+@app.route('/<string:category>/<string:query>', methods=['GET'])
+@app.route('/<string:query>', methods=['GET'])
+def redirect_with_filter(category='', query=''):
+    """Redirects to a multistre.am URL based on filter properties.
+
+    Arguments:
+        category:   (str) category by which to filter, e.g. status, game, profile
+        query:      (str) filter to use [OPTIONAL]
+
+    If no category argument is given the default behavior is to query by streamers status.
+    """
+
+    category_dict = {'status': filter_by_status,
+                     's': filter_by_status,
+                     'game': filter_by_game,
+                     'g': filter_by_game}
+
+    if not category:
+        # TODO: do something more useful here
+        redirect_url = filter_by_status(query)
+        return 'redirect url: %s' % redirect_url
+
+    if category in category_dict.keys():
+        redirect_url = category_dict[category](query)
+        # return 'redirect url: %s' % redirect_url
+        return redirect(redirect_url, code=303)
+
+    return 'default\ncategory: %s\nquery: %s' % (category, query)
+
+
+def exception_handler(request, exception):
+    return 'Error processing request. Try refreshing.'
+
+
+def filter_by_status(status_query):
+    """Filter default streamer list by substring in status."""
+
+    live_list = []
+
+    streamers = [streamer for streamer in app.config['STREAMERS']]
+
+    urls = [('https://api.twitch.tv/kraken/streams/%s' % streamer) for streamer in streamers]
+    headers = {'Accept': 'application/vnd.twitchtv.v3+json',
+               'Client-ID': app.config['CLIENT_ID']}
+    requests = (grequests.get(url, headers=headers) for url in urls)
+    response = grequests.map(requests, exception_handler=exception_handler)
+
+    for index, value in enumerate(response):
+        data = value.json()
+        if data['stream'] is not None:
+            if status_query.lower() in data['stream']['channel']['status'].lower():
                 live_list.append(streamers[index])
 
     new_url = 'http://multistre.am/%s' % ('/'.join(user for user in live_list))
 
-    # return jsonify({'live': live_list})
-    return redirect(new_url, code=303)
+    return new_url
 
 
-def exception_handler(request, exception):
-        return 'Error processing request. Try refreshing.'
+def filter_by_game(game_query):
+    """Filter default streamer list by exact matching game name."""
+
+    live_list = []
+
+    streamers = [streamer for streamer in app.config['STREAMERS']]
+
+    urls = [('https://api.twitch.tv/kraken/streams/%s' % streamer) for streamer in streamers]
+    headers = {'Accept': 'application/vnd.twitchtv.v3+json',
+               'Client-ID': app.config['CLIENT_ID']}
+    requests = (grequests.get(url, headers=headers) for url in urls)
+    response = grequests.map(requests, exception_handler=exception_handler)
+
+    for index, value in enumerate(response):
+        data = value.json()
+        if data['stream'] is not None:
+            if game_query.lower() == data['stream']['channel']['game'].lower():
+                live_list.append(streamers[index])
+
+    new_url = 'http://multistre.am/%s' % ('/'.join(user for user in live_list))
+
+    return new_url
